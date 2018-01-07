7cf1b1f0d1b8b34fc32f5d3add69f561e094da28
tanxiangyuan <tanxiangyuan@jd.com>
2018-01-07 18:39:53 +0800

    add: 动态加载模块

--- client/pages/page-1/routes.js
+++ client/pages/page-1/routes.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from '../../common/App';
 import Page from './Page';
 import Module1Com1 from './module-1/components/com-1';
 import Module1Com2 from './module-1/components/com-2';
-import Module2Com1 from './module-2/components/com-1';
 
 const routes = (
   <Route path="/" component={App}>
@@ -12,7 +11,11 @@ const routes = (
       <Route path="module1" component={Module1Com1}>
         <IndexRoute components={Module1Com2}/>
       </Route>
-      <Route path="module2" component={Module2Com1} />
+      <Route path="module2"
+             getComponent={(nextState, cb) => {
+               //动态加载模块
+               import('./module-2/components/com-1').then((_thunk) => cb(null, _thunk.default));
+             }}/>
     </Route>
   </Route>
 );
