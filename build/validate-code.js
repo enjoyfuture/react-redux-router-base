e34144375e488280088336d95e600678fb6ec4c5
wangyanjun <linder0209@126.com>
2018-08-30 09:49:03 +0800

    优化 validate-code 写法

--- build/validate-code.js
+++ build/validate-code.js
@@ -25,15 +25,15 @@ async function runEsLint() {
   // 标记是否通过检测
   let pass = 0;
   // --cached 表示暂存区，即执行 git add 后的文件
-  const res = await exec('git diff --cached --name-only| grep .js$').catch(
+  const result = await exec('git diff --cached --name-only| grep .js$').catch(
     error => console.log(chalk.green('\ngit 暂存区没有要提交的 js 文件'))
   );
 
-  if (!res) {
+  if (!result) {
     return Promise.resolve(0);
   }
 
-  const { stdout, stderr } = res;
+  const { stdout, stderr } = result;
 
   if (stderr) {
     console.log(chalk.red(`exec stderr: ${stderr}`));
@@ -45,16 +45,16 @@ async function runEsLint() {
     const results = cli.executeOnFiles(array).results;
     let errorCount = 0;
     let warningCount = 0;
-    results.forEach(result => {
-      errorCount += result.errorCount;
-      warningCount += result.warningCount;
-      if (result.messages.length > 0) {
+    results.forEach(item => {
+      errorCount += item.errorCount;
+      warningCount += item.warningCount;
+      if (item.messages.length > 0) {
         console.log('\n');
         console.log(
           chalk.cyan('不符合 eslint 规则文件：'),
-          chalk.cyan(result.filePath)
+          chalk.cyan(item.filePath)
         );
-        result.messages.forEach(obj => {
+        item.messages.forEach(obj => {
           const level = getStderrLevel(obj.severity);
           console.log(
             chalk.red(
@@ -84,15 +84,15 @@ async function runStyleLint() {
   // 标记是否通过检测
   let pass = 0;
 
-  const res = await exec('git diff --cached --name-only| grep .scss$').catch(
+  const result = await exec('git diff --cached --name-only| grep .scss$').catch(
     error => console.log(chalk.green('\ngit 暂存区没有要提交的 scss 文件'))
   );
 
-  if (!res) {
+  if (!result) {
     return Promise.resolve(0);
   }
 
-  const { stdout, stderr } = res;
+  const { stdout, stderr } = result;
 
   if (stderr) {
     console.log(chalk.red(`exec stderr: ${stderr}`));
@@ -112,13 +112,13 @@ async function runStyleLint() {
       .then(results => {
         let errorCount = 0;
         let warningCount = 0;
-        JSON.parse(results.output).forEach(result => {
-          if (result.warnings.length > 0) {
+        JSON.parse(results.output).forEach(item => {
+          if (item.warnings.length > 0) {
             console.log('\n');
             console.log(
-              chalk.cyan(`不符合 stylelint 规则文件：${result.source}`)
+              chalk.cyan(`不符合 stylelint 规则文件：${item.source}`)
             );
-            result.warnings.forEach(obj => {
+            item.warnings.forEach(obj => {
               const level = getStderrLevel(obj.severity);
 
               warningCount += obj.severity === 'error' ? 0 : 1;
